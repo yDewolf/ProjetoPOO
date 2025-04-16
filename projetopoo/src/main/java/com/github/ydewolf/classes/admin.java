93af244209c4f04b052f2f6b5c9c7e440536a8ba
DadiAugusto <>
2025-04-16 13:54:10 -0300

    Create admin.java

---
+++ projetopoo/src/main/java/com/github/ydewolf/classes/admin.java
@@ -0,0 +1,51 @@
+package com.github.ydewolf.classes;
+
+import com.github.ydewolf.enums.Setores;
+import com.github.ydewolf.enums.TiposFuncionario;
+
+public class admin extends Intern {
+
+
+    public admin(String name, UserHolder holder, String username, String password, String email, String cpf,
+            String data_nascismento, String telefone, String endereco, Setores setor, double salario,
+            TiposFuncionario tipo) 
+        {
+        super(name, holder, username, password, email, cpf, data_nascismento, telefone, endereco, setor, salario, tipo);
+    }
+
+    public void cadastrarFuncionario(
+        String name,
+        String username,
+        String password,
+        String email,
+        String cpf,
+        String data_nascismento,
+        String telefone,
+        String endereco,
+        Setores setor,
+        double salario,
+        TiposFuncionario tipo
+    ) {
+        this.user_holder.registrarFuncionario(name, username, password, email, cpf, data_nascismento, telefone, endereco, setor, salario, tipo);
+    }
+
+    public void demitirFuncionario() {
+        this.user_holder.removeUser(new UserSession(this), this.username);
+    }
+
+    public void atualizarSalario(String username, double salario) {
+        User usuario = this.user_holder.getUser(username);
+        if (Intern.class.isInstance(usuario)) {
+            ((Intern) usuario).setSalario(salario);
+            System.out.println("Salario atualizado com sucesso!");
+        }
+    }
+
+    public void atualizarSetor(String username, Setores setor) {
+        User usuario = this.user_holder.getUser(username);
+        if (Intern.class.isInstance(usuario)) {
+            ((Intern) usuario).setSetor(setor);
+            System.out.println("Setor atualizado com sucesso!");
+        }
+    }
+}
